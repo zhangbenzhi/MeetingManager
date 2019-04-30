82d2b1ae094828ecaf97882f88ab247a478227e0
zhangbenzhi <zhangbenzhi@dangkegongyu.com>
2019-04-30 18:50:24 +0800

    黄油刀

--- app/src/main/java/com/example/meetingmanager/activity/LoginActivity.java
+++ app/src/main/java/com/example/meetingmanager/activity/LoginActivity.java
@@ -2,10 +2,18 @@ package com.example.meetingmanager.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.meetingmanager.R;
 
+import butterknife.BindView;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_username)
+    EditText etUsername;
+    @BindView(R.id.et_password)
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
